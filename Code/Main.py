19f64a51907ccc3ce1be969f5a6221ac7b11a28b
supker1410 <sup.ker@outlook.com>
2025-06-17 12:32:04 +0000

    gchh

---
+++ Code/Main.py
@@ -0,0 +1,13 @@
+import random
+from os import system
+import time
+import sys
+print("easy mode")
+secenekler = ["aZOnl", "UisVr", "İgMYa", "DnmTy", "DşİuO"]
+seçim = random.choice(secenekler)
+print(seçim)
+time.sleep(1)
+system('cls||clear')
+for line in sys.stdin:
+    line = line.rstrip()
+print("seçim")
